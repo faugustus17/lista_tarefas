edcb80d95710a900805423e2f51202e0c078d1e7
Fernando Guimarães <faugustus17@gmail.com>
2019-06-20 18:36:42 -0300

    Criados os metodos.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(
     MaterialApp(
@@ -13,6 +16,30 @@ class Home extends StatefulWidget{
 }
 
 class _HomeState extends State<Home>{
+
+  //Obter dados
+  Future<String> _readData() async{
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    }catch (e){
+      return null;
+    }
+  }
+
+  //Salvar os dados
+  Future<File> _saveData() async{
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  //Ler o arquivo de dados
+  Future<File> _getFile() async{
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/dados_tarefa.json");
+  }
+
   @override
   Widget build(BuildContext context){
     return Container();
